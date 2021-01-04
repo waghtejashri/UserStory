1c3f85a99acda22cebd81c044b492a87b877ef55
waghtejashri <tejashriwagh812@gmail.com>
2021-01-04 15:50:12 +0530

    adding all the files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"database":"mongodb+srv://root:root@mystory.72gxh.mongodb.net/<dbname>?retryWrites=true&w=majority",
+	"port": process.env.PORT || 3000,
+	"secretKey": "WebLove"
+}
